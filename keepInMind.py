9238adc1890a10fce0407cee0526efd05b6da8a5
dacostafrankaboagye <60321537+dacostafrankaboagye@users.noreply.github.com>
2023-12-25 08:22:58 +0000

    Add files via upload

---
+++ keepInMind.py
@@ -0,0 +1,32 @@
+
+'''
+from collections import Counter 
+
+a = "abaac"
+d = Counter(a)
+
+for k, v in d.items():
+    print(k," : ",v)
+
+'''
+
+'''
+
+Minimu Value of any two pairs in an Array:
+
+- if  you sort an array
+    - the difference of the adjacent 
+        - this can help you find the minimum value of any two pairs
+
+
+'''
+
+'''
+combination of any two pairs =  nC2
+( n * (n-1) ) // 2
+'''
+
+
+import math
+
+math.fabs
