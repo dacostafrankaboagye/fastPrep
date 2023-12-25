cdd64e466edd2656abf32d7562baae288995d313
dacostafrankaboagye <60321537+dacostafrankaboagye@users.noreply.github.com>
2023-12-25 10:21:42 +0000

    Update EqualStack.java

--- stacks/EqualStack.java
+++ stacks/EqualStack.java
@@ -1,7 +1,10 @@
 /*
-You have three stacks of cylinders where each cylinder has the same diameter, but they may vary in height. You can change the height of a stack by removing and discarding its topmost cylinder any number of times.
+You have three stacks of cylinders where each cylinder has the same diameter, but they may vary in height. 
+You can change the height of a stack by removing and discarding its topmost cylinder any number of times.
 
-Find the maximum possible height of the stacks such that all of the stacks are exactly the same height. This means you must remove zero or more cylinders from the top of zero or more of the three stacks until they are all the same height, then return the height.
+Find the maximum possible height of the stacks such that all of the stacks are exactly the same height. 
+This means you must remove zero or more cylinders from the top of zero or more of the three stacks 
+until they are all the same height, then return the height.
 
 Example
 h1 = [1,2,1,1]
@@ -9,21 +12,26 @@ h2 = [1,1,2]
 h3 = [1,1]
 
 
-There are 4, 3, and 2 cylinders in the three stacks, with their heights in the three arrays. Remove the top 2 cylinders from h1 (heights = [1, 2]) and from h2 (heights = [1, 1]) so that the three stacks all are 2 units tall. Return 2 as the answer.
+There are 4, 3, and 2 cylinders in the three stacks, with their heights in the three arrays.
+Remove the top 2 cylinders from h1 (heights = [1, 2]) and from h2 (heights = [1, 1]) 
+so that the three stacks all are 2 units tall. Return 2 as the answer.
 
 Note: An empty stack is still a stack.
 
 
-You have three stacks of cylinders where each cylinder has the same diameter, but they may vary in height. You can change the height of a stack by removing and discarding its topmost cylinder any number of times.
+You have three stacks of cylinders where each cylinder has the same diameter,
+but they may vary in height. You can change the height of a stack by removing
+and discarding its topmost cylinder any number of times.
 
-Find the maximum possible height of the stacks such that all of the stacks are exactly the same height. This means you must remove zero or more cylinders from the top of zero or more of the three stacks until they are all the same height, then return the height.
+Find the maximum possible height of the stacks such that all of the stacks are
+exactly the same height. This means you must remove zero or more cylinders from the
+top of zero or more of the three stacks until they are all the same height, then return the height.
 
 Example
 
-
-
-
-There are  and  cylinders in the three stacks, with their heights in the three arrays. Remove the top 2 cylinders from  (heights = [1, 2]) and from  (heights = [1, 1]) so that the three stacks all are 2 units tall. Return  as the answer.
+There are  and  cylinders in the three stacks, with their heights in the three arrays. 
+Remove the top 2 cylinders from  (heights = [1, 2]) and from  (heights = [1, 1]) 
+so that the three stacks all are 2 units tall. Return  as the answer.
 
 Note: An empty stack is still a stack.
 
@@ -36,20 +44,11 @@ equalStacks has the following parameters:
 int h1[n1]: the first array of heights
 int h2[n2]: the second array of heights
 int h3[n3]: the third array of heights
+
 Returns
-
 int: the height of the stacks when they are equalized
-Input Format
-
-The first line contains three space-separated integers, , , and , the numbers of cylinders in stacks , , and . The subsequent lines describe the respective heights of each cylinder in a stack from top to bottom:
-
-The second line contains  space-separated integers, the cylinder heights in stack . The first element is the top cylinder of the stack.
-The third line contains  space-separated integers, the cylinder heights in stack . The first element is the top cylinder of the stack.
-The fourth line contains  space-separated integers, the cylinder heights in stack . The first element is the top cylinder of the stack.
-Constraints
 
 Sample Input
-
 STDIN       Function
 -----       --------
 5 3 4       h1[] size n1 = 5, h2[] size n2 = 3, h3[] size n3 = 4  
@@ -57,13 +56,17 @@ STDIN       Function
 4 3 2       h2 = [4, 3, 2]
 1 1 4 1     h3 = [1, 1, 4, 1]
 
-
 Sample Output
 5
-
-
 */
 
+/*
+
+ - To find the solution we need to maximize this height and that should be the answer to this problem.
+ - We can create 3 new stacks with the cumulative sums of the cylinder heights in each stack. 
+ - This way the top element of each stack would tell you the current height of the stack
+ - go on removing the highest element from each of the 3 stacks until the top element of all the stacks is same.
+*/
 
 
 
