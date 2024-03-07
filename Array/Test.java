5f1e2ccc64907e5185c59786a5cc15519c95adf4
dacostafrankaboagye <frank.aboagye@ashesi.edu.gh>
2024-03-07 17:55:55 +0000

    solved getConcatenation- array

--- Array/Test.java
+++ Array/Test.java
@@ -12,11 +12,29 @@ class Solution {
         
     
     }
+
+    public int[] getConcatenation(int[] nums) {
+        int nums_length = nums.length;
+        int[] result = new int[nums_length*2];
+        for(int i=0; i<nums_length; i++){
+            result[i] = nums[i];
+            result[nums_length+i] = nums[i];
+        }
+        return result;
+        
+    }
 }
 
 public class Test{
 
     public static void main(String[] args) {
+
+        Solution s = new Solution();
+        int[] test = {1,2,3};
+        int[] ans = s.getConcatenation(test);
+        for(int x : ans){
+            System.out.println(x);
+        }
         
         
     }
